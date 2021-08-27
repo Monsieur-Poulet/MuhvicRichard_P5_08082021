ffd4374feda66317b0ccae10bb58dafbb5e9bbdc
Richard <richardmuhvic@gmail.com>
2021-08-27 12:58:09 +0200

    mise en place de styles.css et ajotu des premiers éléments de style

--- frontend/main.js
+++ frontend/main.js
@@ -1,1 +1,10 @@
-const dataApi=fetch("http://localhost:3000/api/cameras")
+let camera;
+
+//API request
+const fetchCamera = async() => {
+    camera = await fetch('http://localhost:3000/api/cameras').then(res => res.json());
+
+    console.log(camera);
+};
+
+fetchCamera();
