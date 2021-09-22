282ef82f9a621d72c5c3427e1e844620dde3c741
Richard <richardmuhvic@gmail.com>
2021-09-22 12:11:42 +0200

    fin de la page produc js

--- frontend/product.js
+++ frontend/product.js
@@ -7,26 +7,31 @@ fetch('http://localhost:3000/api/cameras')
      displayProduct(data)
  });
 
-
+//création de la fonction 'product' :
 function displayProduct(product) {
     let elementContainer = document.getElementById('container');
 
     for(let i=0; i<product.length; i++) {
         console.log(product[i]);
 
-        let numberEnEuro = product[i].price;
+        //je passe le prix à la décimale :
+        let numberEnEuro = product[i].price ;
         numberEnEuro /= 100;
 
+        //création d'une nouvelle div avec la class 'caméra" :
         let newDiv = document.createElement("div");
         newDiv.classList.add("camera");
+
+        //création de la carte :
         newDiv.innerHTML +=
             `<div class="card">
                 <img id="image" src=${product[i].imageUrl} alt="image de la caméra un">
-                <div id="price">
+                <a href="" id="price">
                     <h2 class="name_product" >${product[i].name}</h2>
                     <p class="price" >${numberEnEuro.toFixed(2)}</p>
-                </div>
+                </a>
             </div>`;
+        //Parenter la div au container :
         elementContainer.appendChild(newDiv);
     }
 console.log(elementContainer);
