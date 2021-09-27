4c22d289765c5bf6087468f24573753d46c77be0
Richard <richardmuhvic@gmail.com>
2021-09-27 18:15:16 +0200

    création de la page produit

--- frontend/product.js
+++ frontend/product.js
@@ -1,4 +1,13 @@
-fetch('http://localhost:3000/api/cameras')
+/*
+- Faire une redirection au click sur un carte(ca tu sais déjà le faire) index.js/html
+- Créer la page html qui va être ta page produit product.html
+- Faire un appel à l'api(sur le même principe que sur la page index mais la route est différente) pour récupérer les infos d'un seul produit  product.js 
+- Génèrer le html comme tu as fait sur la page index.  product.js 
+*/
+
+
+//fetch du produit ciblé :
+fetch('http://localhost:3000/api/cameras/')
  .then( function(response) {
      return response.json();
  })
@@ -7,36 +16,33 @@ fetch('http://localhost:3000/api/cameras')
      displayProduct(data)
  });
 
-//création de la fonction 'product' :
+
+
+// Utiliser URLsearchParam?
+// Utiliser le signe '?' pour lier l'ID en fonction de la carte choisie? 
+
+
+//la fonction doit envoyer au produit sélectionné 
+//dans la carte de la page index.html :
 function displayProduct(product) {
-    let elementContainer = document.getElementById('container');
+    let elementContainer = document.getElementById('product');
+    //je passe le prix à la décimale :
+    let numberEnEuro = product[i].price ;
+    numberEnEuro /= 100;
 
-    for(let i=0; i<product.length; i++) {
-        console.log(product[i]);
-
-        //je passe le prix à la décimale :
-        let numberEnEuro = product[i].price ;
-        numberEnEuro /= 100;
-
-        //création d'une nouvelle div avec la class 'caméra" :
-        let newDiv = document.createElement("div");
-        newDiv.classList.add("camera");
-
-        //création de la carte :
-        newDiv.innerHTML +=
-            `<div class="card">
-                <img id="image" src=${product[i].imageUrl} alt="image de la caméra un">
-                <a href="" id="price">
-                    <h2 class="name_product" >${product[i].name}</h2>
-                    <p class="price" >${numberEnEuro.toFixed(2)}</p>
-                </a>
-            </div>`;
-        //Parenter la div au container :
-        elementContainer.appendChild(newDiv);
-    }
-console.log(elementContainer);
+    //création de la carte produit choisi :
+    product.innerHTML +=
+    `<div>
+        <img src=${product[i].imageUrl} alt="image de la caméra">
+        <div class="description_produit">
+            <h2>${product[i].name}</h2>
+            <p>${product[i].description}</p>
+            <p class="price">${numberEnEuro.toFixed(2)}</p>
+            <p>Quantité :</p>
+        </div>
+        <div class="btn">
+            <a href="index.html" id="back">Revenir aux caméras</a>
+            <a href="panier.html" id="panier">Ajouter au panier</a>
+        </div>
+    </div>`
 }
-
-
-
-
